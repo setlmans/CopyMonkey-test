c70c84bd762009c42bb6da00215d7a9305ef85cd
Nikita Shchekoldin <schekoldin@reksoft.ru>
2021-10-06 21:27:10 +0300

    create repo

---
+++ lambda_get.py
@@ -0,0 +1,15 @@
+import boto3
+from boto3.dynamodb.conditions import Key
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('post_request')
+    store = 'messages'
+    response = table.query(
+        KeyConditionExpression=Key('store').eq(store),
+        Limit=10,
+        ScanIndexForward=False
+    )
+    # print(type(response))
+    return response['Items']
+
---
+++ lambda_post.py
@@ -0,0 +1,23 @@
+import boto3
+import json
+import time
+
+def lambda_handler(event, context):
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('post_requests')
+    print("Received event: " + json.dumps(event))
+    message = json.loads(event['body'])['message']
+
+    table.put_item(
+        Item={
+            'store': 'messages',
+            'message': message,
+            'unix_time': int(time.time())
+
+        }
+    )
+    return {
+        'statusCode': 200,
+        'body': "written"
+    }
